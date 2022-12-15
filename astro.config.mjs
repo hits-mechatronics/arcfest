b90af7516cd8ef54f8653c7d578bff64d8861f91
lucidmach <lucidmach@gmail.com>
2022-12-15 17:17:58 +0530

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel(),
 });
