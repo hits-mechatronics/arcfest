6fea98f9c6dc4df27afc736e649cc84fe5cc60af
lucidmach <lucidmach@gmail.com>
2022-12-22 16:51:49 +0530

    faq page

---
+++ src/components/Question.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+interface props {
+  title: string;
+  content: string;
+}
+
+const Question: React.FC<props> = ({ title, content }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setOpen((open) => !open);
+  };
+
+  return (
+    <div className="min-w-[360px] w-10/12">
+      <div
+        className="w-full bg-gray-900 px-4 py-2 flex justify-between hover:cursor-pointer"
+        onClick={(e) => handleClick(e)}
+      >
+        <span>{title}</span>
+        <span className="font-black text-lg">{open ? "-" : "+"}</span>
+      </div>
+      {open ? (
+        <div className="w-full bg-gray-900 opacity-75 px-4 py-2">{content}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Question;
---
+++ src/components/Questions.tsx
@@ -0,0 +1,23 @@
+import type { ReactHTMLElement } from "react";
+import qs from "../json/faq.json";
+import Question from "./Question";
+
+const renderQs = () => {
+  const QList: JSX.Element[] = [];
+
+  qs.map((q, i) => {
+    QList.push(<Question title={q.q} content={q.a} key={i} />);
+  });
+
+  return QList;
+};
+
+const Questions: React.FC = () => {
+  return (
+    <div className="w-screen h-full flex flex-col justify-center items-center gap-4">
+      {renderQs()}
+    </div>
+  );
+};
+
+export default Questions;
