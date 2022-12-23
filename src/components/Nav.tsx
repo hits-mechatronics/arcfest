8e2214c06f96e64305bb96bd3dcdfdc59c62b205
lucidmach <lucidmach@gmail.com>
2022-12-23 16:32:43 +0530

    layout fixes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -30,17 +30,17 @@ const Nav: React.FC<props> = ({ currentRoute }) => {
         <nav className="rounded-full bg-gray-900">
           <ul className="flex">
             {routes.map((item) => (
-              <li
-                key={item.name}
-                className={clsx(
-                  "no-underline py-2 px-4 rounded-[6rem] lg:py-4 lg:px-8",
-                  item.route === currentRoute && "bg-black"
-                )}
-              >
-                <a href={item.route} className="no-underline" rel="prefetch">
+              <a href={item.route} className="no-underline" rel="prefetch">
+                <li
+                  key={item.name}
+                  className={clsx(
+                    "no-underline py-2 px-4 rounded-[6rem] lg:py-4 lg:px-8",
+                    item.route === currentRoute && "bg-black"
+                  )}
+                >
                   {item.name}
-                </a>
-              </li>
+                </li>
+              </a>
             ))}
           </ul>
         </nav>
